79fdec7b003f9fc5bf1d5bbe08bee5b3241288ac
Michel <michel.hinostroza@gmail.com>
2020-11-24 10:56:37 -0500

    add html editor

--- noolvidemospe/urls.py
+++ noolvidemospe/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+admin.site.site_header = 'NO OLVIDEMOS'
+admin.site.site_title = 'NO OLVIDEMOS'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
